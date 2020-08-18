28f5e0a4d194970bb9474d6ab94544981e8f47db
Ryan Hyma <ryan.hyma@gmail.com>
2020-08-18 12:37:38 -0400

    Update to decrypt

--- decrypt.sh
+++ decrypt.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 export GPG_TTY=$(tty)
-echo ls -la
-echo $CONFIG_INI_PASSPHRASE
-gpg --quiet --yes --decrypt --passphrase="$CONFIG_INI_PASSPHRASE" \
+ls -la
+gpg --quiet --batch --yes --decrypt --passphrase="$CONFIG_INI_PASSPHRASE" \
 --output config.ini config.ini.gpg
