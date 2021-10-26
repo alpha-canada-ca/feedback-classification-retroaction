e6009997c0dc2b72dada8217cd70e08d39449f80
Hamza Aburaneh <Hamza.Aburaneh@carleton.ca>
2021-10-26 14:17:56 -0400

    adding ircc Base

--- fetch_table.py
+++ fetch_table.py
@@ -9,35 +9,44 @@ from configparser import ConfigParser
 config = ConfigParser()
 config.read('config/config.ini')
 key = config.get('default', 'api_key')
-base = config.get('default', 'base')
-base_health= config.get('default', 'base_health')
-base_cra= config.get('default', 'base_cra')
-base_travel= config.get('default', 'base_travel')
-airtable_main = Airtable(base, 'Page feedback', api_key=key)
+
+base        = config.get('default', 'base')
+base_health = config.get('default', 'base_health')
+base_cra    = config.get('default', 'base_cra')
+base_travel = config.get('default', 'base_travel')
+base_ircc   = config.get('default', 'base_ircc')
+
+airtable_main   = Airtable(base, 'Page feedback', api_key=key)
 airtable_health = Airtable(base_health, 'Page feedback', api_key=key)
-airtable_cra = Airtable(base_cra, 'Page feedback', api_key=key)
+airtable_cra    = Airtable(base_cra, 'Page feedback', api_key=key)
 airtable_travel = Airtable(base_travel, 'Page feedback', api_key=key)
+airtable_ircc   = Airtable(base_ircc, 'Page feedback', api_key=key)
 
 
 
 print('Accessed the key')
-record_list_main = airtable_main.get_all()
-record_list_health = airtable_health.get_all()
-record_list_cra = airtable_cra.get_all()
-record_list_travel = airtable_travel.get_all()
+record_list_main    = airtable_main.get_all()
+record_list_health  = airtable_health.get_all()
+record_list_cra     = airtable_cra.get_all()
+record_list_travel  = airtable_travel.get_all()
+record_list_ircc    = airtable_ircc.get_all()
 
 print('Fetched the data')
 #convert data to Pandas dataframe
-data_main = pd.DataFrame([record['fields'] for record in record_list_main])
+data_main   = pd.DataFrame([record['fields'] for record in record_list_main])
 data_health = pd.DataFrame([record['fields'] for record in record_list_health])
-data_cra = pd.DataFrame([record['fields'] for record in record_list_cra])
+data_cra    = pd.DataFrame([record['fields'] for record in record_list_cra])
 data_travel = pd.DataFrame([record['fields'] for record in record_list_travel])
+data_ircc   = pd.DataFrame([record['fields'] for record in record_list_ircc])
 
-data_2 = data_main.append(data_health, ignore_index=True, sort=True)
 
-data_1 = data_2.append(data_cra, ignore_index=True, sort=True)
+data_3  = data_1.append(data_travel, ignore_index=True, sort=True)
 
-data = data_1.append(data_travel, ignore_index=True, sort=True)
+data_2  = data_main.append(data_health, ignore_index=True, sort=True)
+
+data_1  = data_2.append(data_cra, ignore_index=True, sort=True)
+
+data    = data_3.append(data_ircc, ignore_index=True, sort=True)
 
 
 data = data[["Unique ID", "Comment", "Date", "Status", "Lookup_tags", 'Tags confirmed', 'Lookup_page_title', 'URL_function', 'Lookup_FR_tag', "Lookup_group_EN", "Lookup_group_FR", "Lang", "What's wrong"]]
