953610de00af70297aee5228f29fef397daf0028
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2023-02-28 11:54:30 -0500

    Improved readability

--- fetch_table.py
+++ fetch_table.py
@@ -5,57 +5,14 @@ import gzip
 import pickletools
 from configparser import ConfigParser
 
-#get api key from config file and get data from AirTabe
-config = ConfigParser()
-config.read('config/config.ini')
-key = config.get('default', 'api_key')
 
-base        = config.get('default', 'base')
-base_health = config.get('default', 'base_health')
-base_cra    = config.get('default', 'base_cra')
-base_travel = config.get('default', 'base_travel')
-base_ircc   = config.get('default', 'base_ircc')
+def filter_data(data):
+    return data[
+        (data.Status != 'Spam') &
+        (data.Status != 'Ignore') &
+        (data.Status != 'Duplicate')
+    ]
 
-airtable_main   = Airtable(base, 'Page feedback', api_key=key)
-airtable_health = Airtable(base_health, 'Page feedback', api_key=key)
-airtable_cra    = Airtable(base_cra, 'Page feedback', api_key=key)
-airtable_travel = Airtable(base_travel, 'Page feedback', api_key=key)
-airtable_ircc   = Airtable(base_ircc, 'Page feedback', api_key=key)
-
-
-
-print('Accessed the key')
-record_list_main    = airtable_main.get_all()
-record_list_health  = airtable_health.get_all()
-record_list_cra     = airtable_cra.get_all()
-record_list_travel  = airtable_travel.get_all()
-record_list_ircc    = airtable_ircc.get_all()
-
-print('Fetched the data')
-#convert data to Pandas dataframe
-data_main   = pd.DataFrame([record['fields'] for record in record_list_main])
-data_health = pd.DataFrame([record['fields'] for record in record_list_health])
-data_cra    = pd.DataFrame([record['fields'] for record in record_list_cra])
-data_travel = pd.DataFrame([record['fields'] for record in record_list_travel])
-data_ircc   = pd.DataFrame([record['fields'] for record in record_list_ircc])
-
-
-data_1  = data_main.append(data_health, ignore_index=True, sort=True)
-
-data_2  = data_1.append(data_travel, ignore_index=True, sort=True)
-
-data_3  = data_2.append(data_cra, ignore_index=True, sort=True)
-
-data    = data_3.append(data_ircc, ignore_index=True, sort=True)
-
-
-data = data[["Unique ID", "Comment", "Date", "Status", "Lookup_tags", 'Tags confirmed', 'Lookup_page_title', 'URL_function', 'Lookup_FR_tag', "Lookup_group_EN", "Lookup_group_FR", "Lang", "What's wrong"]]
-data = data[data.Status != 'Spam']
-data = data[data.Status != 'Ignore']
-data = data[data.Status != 'Duplicate']
-
-
-print('Created the dataframes')
 
 def serialize(obj, file, protocol=-1):
     with gzip.open(file, "wb") as f:
@@ -64,7 +21,30 @@ def serialize(obj, file, protocol=-1):
         f.write(optimized_pickle)
 
 
-#save data as a pickle
+config = ConfigParser()
+config.read('config/config.ini')
+key = config.get('default', 'api_key')
+bases = [
+    config.get('default', 'base'),
+    config.get('default', 'base_health'),
+    config.get('default', 'base_cra'),
+    config.get('default', 'base_travel'),
+    config.get('default', 'base_ircc')
+]
+
+airtables = [Airtable(base, 'Page feedback', api_key=key) for base in bases]
+
+print('Accessed the key')
+record_lists = [airtable.get_all() for airtable in airtables]
+
+print('Fetched the data')
+data = pd.concat([pd.DataFrame([record['fields'] for record in record_list]) for record_list in record_lists], ignore_index=True, sort=True)
+data = data[["Unique ID", "Comment", "Date", "Status", "Lookup_tags", 'Tags confirmed', 'Lookup_page_title', 'URL_function', 'Lookup_FR_tag', "Lookup_group_EN", "Lookup_group_FR", "Lang", "What's wrong"]]
+data = filter_data(data)
+
+print('Created the dataframes')
+
+# save data as a pickle
 serialize(data, 'data/all_data.pickle')
 
 print('Saved dataframes as pickle files')
