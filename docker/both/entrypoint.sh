a697ef0c63cb24c118b3b2299381b4ca3898bff5
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2024-01-22 13:44:19 -0500

    update dependencies

--- docker/both/entrypoint.sh
+++ docker/both/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cron
-python /app/process_feedback.py &  # Run the script in the background
+echo "Running process_feedback.py"
+echo "Python Path: $(which python3)"
+echo "PATH: $PATH"
+python3 -m /app/process_feedback.py
+echo "Starting Flask app"
 flask run --host 0.0.0.0
--- process_feedback.py
+++ process_feedback.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # import libraries
 import os
 from pyairtable import Api
