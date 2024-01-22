036b5fed7328682a519feb9efd90c4a0a9a77048
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2024-01-22 13:49:00 -0500

    Update entrypoint.sh

--- docker/both/entrypoint.sh
+++ docker/both/entrypoint.sh
@@ -2,6 +2,6 @@
 echo "Running process_feedback.py"
 echo "Python Path: $(which python3)"
 echo "PATH: $PATH"
-python3 -m /app/process_feedback.py
+python3 /app/process_feedback.py
 echo "Starting Flask app"
 flask run --host 0.0.0.0
